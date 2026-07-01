30abe569b79aef3968249879d23217d9149ddfc2
PRITAM KUMAR <pritamku3326@gmail.com>
2026-07-01 20:59:36 +0530

    .env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://pritamku3326:9999707672@cluster0.ydvmzvv.mongodb.net/food-del');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("DB Connected to MongoDB Atlas");
   } catch (error) {
     console.error("Failed to connect to MongoDB Atlas:", error.message);
